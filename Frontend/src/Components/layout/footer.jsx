952e725fa6dd66209269315e28506d928e38b8bf
dullcoder993 <prathmeshkulkarni9529@gmail.com>
2026-01-17 19:01:52 +0530

    remove node_modules from repo

---
+++ Frontend/src/Components/layout/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-gray-900 text-white p-3'>
+      <h1>Footer</h1>
+    </div>
+  )
+}
+
+export default Footer
