af56f123eb5a8971d32151263ee4314a1910dc27
dullcoder993 <prathmeshkulkarni9529@gmail.com>
2026-01-26 12:04:30 +0530

    V-2

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -47,7 +47,7 @@ app.use(express.static(path.join(__dirname, "dist")));
 
 // SPA fallback
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname,"..", "dist", "index.html"));
 });
 
 export { app }
