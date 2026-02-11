051fdcc147576bcd2a713cf52e8f1b780e66519b
dullcoder993 <prathmeshkulkarni9529@gmail.com>
2026-02-11 23:26:47 +0530

    Debug

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,8 @@ const app  = express()
 
 app.use(cors({
   origin: ["http://localhost:5173",
-    "https://e-commerce-3-ykpe.onrender.com"
+    "https://e-commerce-3-ykpe.onrender.com",
+    "https://e-commerce-2-vgum.onrender.com/"
   ],
 
   methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
